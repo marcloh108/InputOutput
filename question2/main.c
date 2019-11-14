c38bc5a760bb759821d42e4b6236b6e301ca8661
marcloh108 <marc.loh@molisys.com>
2019-11-14 14:00:21 +0700

    Write a C program to find the third angle of a triangle if two angles are given
    Create functions for define and printing
    
    Signed-off-by: marcloh108 <marc.loh@molisys.com>

--- question2/main.c
+++ question2/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int a, b, c, sum;
-    printf("Input three numbers separated by comma :");
-    scanf("%d, %d, %d", &a, &b, &c);
-    sum = a + b + c;
-    printf("The sum of three numbers : %d", sum);
+    int a, b, c;
+    printf("Input two angles of triangle separated by comma: ");
+    scanf("%d, %d", &a, &b);
+    c = 180 - a - b ;
+    printf("Third angle of the triangle : %d", c);
     return(0);
 }
